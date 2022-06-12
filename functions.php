3d4fbf72a7464e12cbcade7d641885b65b6e6e10
dollarstir <kpin463@gmail.com>
2022-06-12 07:15:43 +0000

    wokring on login

--- functions.php
+++ functions.php
@@ -19,7 +19,19 @@ function login()
     $password = mysqli_real_escape_string($conn, $password);
 
     $sel = mysqli_query($conn, "SELECT * FROM users WHERE email = '$email'");
+    $sel2 = mysqli_query($conn, "SELECT * FROM users WHERE email = '$emal' AND password = '$password'");
+
     if (mysqli_num_rows($sel) >= 1) {
+        if (mysqli_num_rows($sel2) >= 1) {
+            $row = mysqli_fetch_array($sel2);
+            session_start();
+            $_SESSION['id'] = $row['id'];
+
+            echo '<script>alert("Login successful");
+            window.location="dashboard.php";
+            </script>';
+        } else {
+        }
     } else {
         echo 'User does not exist';
     }
