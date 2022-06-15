b795be51963ac04a63f0255d6d33397bb7e9ad31
dollarstir <kpin463@gmail.com>
2022-06-15 20:46:56 +0000

    sfd

--- yolksms.php
+++ yolksms.php
@@ -15,7 +15,7 @@ class Yolksms
 
     public function sms($senderid, $recipient, $message)
     {
-        $url = 'https://apps.mnotify.net/smsapi?key=gThvFfUcm3mf271OGq1uLkWNz&to=0556676471&msg=dollarstir&sender_id=TUCEE HUB';
+        $url = 'http://apps.mnotify.net/smsapi?key=gThvFfUcm3mf271OGq1uLkWNz&to=0556676471&msg=dollarstir&sender_id=TUCEE HUB';
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
