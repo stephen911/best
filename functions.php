d22045c5aefa0ebf060eebe77bbf1c7016acc777
dollarstir <kpin463@gmail.com>
2022-06-15 17:21:47 +0000

    dsfsdf

--- functions.php
+++ functions.php
@@ -98,7 +98,7 @@ function updateuser($id, $title, $name, $gender, $email, $contact, $telegram, $l
         </html>';
         // yolk mailer
         // $mym = [$email];
-        $from = 'info@phpyolk.com';
+        $from = ['Tucee', 'info@phpyolk.com'];
         $headers = 'MIME-Version: 1.0'."\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1'."\r\n";
         $headers .= 'From: '.$from;
