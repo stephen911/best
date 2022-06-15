c69c7b73135a8f08ada4ea1d92c55ae42cee472e
stephen911 <stephendappah1@gmail.com>
2022-06-15 00:04:10 +0000

    thanks

--- payment.php
+++ payment.php
@@ -124,7 +124,7 @@ if (isset($_GET['ref'])) {
                                                 </div>
                                                 <div class="media-right">
                                                     '.$yolk->handler().'
-                                                    '.$yolk->payscript($user['title'], $user['name'], $user['email'], $user['contact'], 100, $ref = '').'
+                                                    '.$yolk->payscript($user['title'], $user['name'], $user['email'], $user['contact'], 70, $ref = '').'
                                                     '.$yolk->pay("Pay Now").'
                                                 </div>
                                             </div>
