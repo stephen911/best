d37b385afe87362685286363bef7567cdd406f5a
dollarstir <kpin463@gmail.com>
2022-06-16 05:30:43 +0000

    sdfsd

--- payhandler.php
+++ payhandler.php
@@ -1,7 +1,10 @@
 <?php
+
  include 'functions.php';
  include 'yolkpay.php';
- $yolk =  new YolkPay();
+ include 'yolksms.php';
+$send = new Yolksms();
+ $yolk = new YolkPay();
 
  checker();
  $user = users();
@@ -11,8 +14,10 @@ if (isset($_GET['ref'])) {
     extract($_GET);
 
     $uid = $_SESSION['id'];
+    include 'starter.php';
+    $sel = mysqli_query($conn, "SELECT * FROM users WHERE id = '$uid'");
+    $row = mysqli_fetch_array($sel);
 
     payment($uid, $ref, $amount);
     echo  '<script>window.location="payment.php"</script>';
 }
-?>
