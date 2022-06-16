827adf9c1c117fda466a111069810bdaa152c082
dollarstir <kpin463@gmail.com>
2022-06-16 10:12:40 +0000

    sdad

--- processor.js
+++ processor.js
@@ -13,6 +13,8 @@ $(function(){
       });
    })
 
+   var re = /\S+@\S+\.\S+/;
+
     $(document).on('click', "#ddo",function(){
         swal({
             title: "Done!",
@@ -188,6 +190,24 @@ $(function(){
               });
 
         }
+
+        // var re = /\S+@\S+\.\S+/ ;
+        else if(re.test(response)){
+
+          swal({
+              title: "Success",
+              html: "Registration Successful. <br>Make Payment to Confirm Resgistration",
+              timer: 3000,
+              type: 'success',
+              padding: "2em",
+              onOpen: function () {
+                swal.showLoading();
+              },
+            }).then(function (result) {
+              window.location.reload();
+            });
+
+      }
 
 
 
