e824942790b44f9d519070779782fdb2e65019ad
stephen911 <stephendappah1@gmail.com>
2022-06-16 13:06:15 +0000

    done

---
+++ admin/delete_user.php
@@ -0,0 +1,13 @@
+<?php
+include "starter.php";
+$id = $_GET['id'];
+$del = mysqli_query($conn, "DELETE FROM users WHERE id = '&id'");
+
+if($del){
+    echo 'deleted';
+}else{
+    echo 'unable to delete';
+
+}
+
+?>
