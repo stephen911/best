ada5e91128c4c81d2d04d666667f80edf3b7caa5
dollarstir <kpin463@gmail.com>
2022-06-16 09:42:03 +0000

    asdasdas

--- processor.php
+++ processor.php
@@ -18,14 +18,19 @@ require 'functions.php';
 
         case 'register':
             extract($_POST);
-            $name = $fname.' '.$lname;
-            if ($password != $repass) {
-                echo 'Password do not match';
-            } elseif ($fname == '' || $lname == '' || $email == '' || $password == '') {
-                echo ' All field must be filled';
+            if ($oname == '') {
+                $name = $fname.' '.$lname;
             } else {
-                register($name, $email, $password);
+                $name = $fname.' '.$oname.' '.$lname;
             }
+                if ($password != $repass) {
+                    echo 'Password do not match';
+                } elseif ($fname == '' || $lname == '' || $email == '' || $password == '') {
+                    echo ' All field must be filled';
+                } else {
+                    register($name, $email, $password);
+                }
+
             break;
 
         case 'changepass':
