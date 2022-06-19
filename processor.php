69f70bff2c434e67646d5289ccb4825a2de82e10
stephen911 <stephendappah1@gmail.com>
2022-06-19 21:02:22 +0000

    done

---
+++ processor.php
@@ -0,0 +1,50 @@
+<?php
+
+// require '../loader/autoloader.php';
+require 'functions.php';
+ if (isset($_GET['action'])) {
+     switch ($_GET['action']) {
+        case 'update':
+            extract($_POST);
+            // extract($_POST);
+            updateuser($id, $title, $name, $gender, $email, $contact, $telegram, $lincesed, $nameofschool, $region, $district, $foodpref, $heard, $tdate);
+
+            break;
+
+        case 'login':
+            extract($_POST);
+            login($email, $password);
+            break;
+
+        case 'register':
+            extract($_POST);
+            if ($oname == '') {
+                $name = $fname.' '.$lname;
+            } else {
+                $name = $fname.' '.$oname.' '.$lname;
+            }
+                if ($password != $repass) {
+                    echo 'Password do not match';
+                } elseif ($fname == '' || $lname == '' || $email == '' || $password == '') {
+                    echo ' All field must be filled';
+                } else {
+                    register($name, $email, $password);
+                }
+
+            break;
+
+        case 'changepass':
+
+            extract($_POST);
+            if ($newpass != $repass) {
+                echo 'Password do not match';
+            } else {
+                changepass($id, $password, $newpass);
+            }
+                break;
+
+        default:
+
+        break;
+    }
+ }
